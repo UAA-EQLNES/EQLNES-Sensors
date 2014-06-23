b18824555688a1dac711997f03a81144546608df
richard-to <gohn76@gmail.com>
2014-06-22 22:56:42 -0800

    Fix minor bug with console output displaying wrong menu option character.
    Improve console output to be slightly more friendly

--- Arduino/libraries/UASensors_SDCard/examples/SDCardTester/SDCardTester.ino
+++ Arduino/libraries/UASensors_SDCard/examples/SDCardTester/SDCardTester.ino
@@ -9,7 +9,7 @@
   - Write file appends to existing content
 
   Created 14 6 2014
-  Modified 14 6 2014
+  Modified 22 6 2014
 */
 #include <UASensors_SDCard.h>
 
@@ -25,13 +25,14 @@ const int CS_PIN = 10;
 const int MESSAGE_SIZE = 101;
 
 // Set the name of the file to be read.
-#define FILENAME "backup.txt"
+#define FILENAME "testfile.txt"
 
 // Instantiate SDCard object. Need to call begin before using.
 UASensors_SDCard sd(CS_PIN);
 
 
 // Menu options recognized from Serial input
+const char SHOW_MENU = 'm';
 const char READ_FILE = 'r';
 const char WRITE_FILE = 'w';
 
@@ -40,10 +41,7 @@ void setup()
 {
   Serial.begin(9600);
 
-  Serial.println("SD Card Tester commands");
-  Serial.println("-----------------------");
-  Serial.println("r: Reads contents of file");
-  Serial.println("f: Write contents to file");
+  doShowMenu();
 
   // Call begin to initialize SD Card
   sd.begin();
@@ -56,6 +54,9 @@ void loop()
   {
     switch (Serial.read())
     {
+      case SHOW_MENU:
+        doShowMenu();
+        break;
       case READ_FILE:
         doReadFile();
         break;
@@ -66,6 +67,15 @@ void loop()
   }
 }
 
+void doShowMenu()
+{
+  Serial.println("SD Card Tester commands");
+  Serial.println("-----------------------");
+  Serial.println("m: Show menu");
+  Serial.println("r: Reads contents of file");
+  Serial.println("w: Write contents to file");
+  Serial.println();
+}
 
 // Reads contents from specified file and returns
 // the contents as String that will be printed to
@@ -78,10 +88,11 @@ void doReadFile()
   {
     Serial.println("Contents of " + String(FILENAME) + ":");
     Serial.println(contents);
+    Serial.println();
   }
   else
   {
-    Serial.println("Notice: No contents found on file.");
+    Serial.println("Notice: No contents found on file.\n");
   }
 }
 
@@ -108,22 +119,26 @@ void doWriteFile()
 
   if (inputLength == 0)
   {
-    Serial.println("Error: No valid date was found!");
+    Serial.println("Error: No valid input was found!\n");
     return;
   }
 
   // Convert char string to string. SDCard writeFile method expects String.
   String message = String(buffer);
 
+  Serial.println("Writing to " + String(FILENAME) + ":");
+  Serial.println(message);
+  Serial.println();
+
   bool isSuccess = sd.writeFile(FILENAME, message);
 
   if (isSuccess)
   {
-    Serial.println("Message written to file successfully!");
+    Serial.println("Message written to file successfully!\n");
   }
   else
   {
-    Serial.println("Error: Could not write message to file!");
+    Serial.println("Error: Could not write message to file!\n");
   }
 }
 
