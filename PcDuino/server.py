933d7b9b6bdc0092001882bb85db6b8c4480b1df
richard-to <gohn76@gmail.com>
2014-07-02 17:31:36 -0800

    Add log file refresh

--- PcDuino/server.py
+++ PcDuino/server.py
@@ -23,6 +23,30 @@ def calculate_date_range(days=7, format="%m/%d/%Y"):
     start_date = end_date - timedelta(days=days)
     return (start_date.strftime("%m/%d/%Y"), end_date.strftime("%m/%d/%Y"))
 
+
+def read_log_file():
+    with open(app.config['DATA_LOGGER_ERROR_FILE'], 'r') as f:
+        f.seek (0, 2)
+        fsize = f.tell()
+        f.seek (max (fsize-5120, 0), 0)
+        lines = f.readlines()
+    log_lines = [line.strip() for line in lines if len(line.strip()) > 0]
+    log = []
+    current_line = None
+    for line in log_lines:
+        if line.startswith('INFO') or line.startswith('ERROR'):
+            if current_line is not None:
+                if current_line.startswith('ERROR'):
+                    log.append(('danger', current_line))
+                else:
+                    log.append(('default', current_line))
+            current_line = line
+        elif current_line is not None:
+            current_line += line
+    log.reverse()
+    return log
+
+
 # Create web server and load settings
 app = Flask(__name__, static_url_path='')
 app.config.from_object('default_settings')
@@ -35,7 +59,7 @@ assets = Environment(app)
 assets.versions = 'timestamp'
 
 
-# Optimize Javascript files
+# Optimize Main javascript files
 js_assets = Bundle(
     'vendor/js/jquery.js',
     'vendor/js/jquery.flot.js',
@@ -49,6 +73,13 @@ js_assets = Bundle(
     filters='rjsmin',
     output='js/min.js')
 assets.register('js_all', js_assets)
+
+# Optimize Log javascript files
+js_log_assets = Bundle(
+    'vendor/js/jquery.js',
+    filters='rjsmin',
+    output='js/jquery.min.js')
+assets.register('js_log', js_log_assets)
 
 
 # Optimize CSS files
@@ -86,25 +117,10 @@ def root():
 
 @app.route('/status')
 def show_status():
-    with open(app.config['DATA_LOGGER_ERROR_FILE'], 'r') as f:
-        f.seek (0, 2)
-        fsize = f.tell()
-        f.seek (max (fsize-5120, 0), 0)
-        lines = f.readlines()
-    log_lines = [line.strip() for line in lines if len(line.strip()) > 0]
-    log = []
-    current_line = None
-    for line in log_lines:
-        if line.startswith('INFO') or line.startswith('ERROR'):
-            if current_line is not None:
-                if current_line.startswith('ERROR'):
-                    log.append(('danger', current_line))
-                else:
-                    log.append(('default', current_line))
-            current_line = line
-        elif current_line is not None:
-            current_line += line
-    return render_template('status.html', site_title=app.config['SITE_TITLE'], log=log)
+    return render_template('status.html',
+        site_title=app.config['SITE_TITLE'],
+        refresh_interval=app.config['REFRESH_INTERVAL'],
+        log=read_log_file())
 
 
 # Endpoint to retrieve data in JSON format
@@ -115,6 +131,12 @@ def fetch_data():
     end_date = request.args.get('end-date', None)
     data = db.fetch(sensor, start_date, end_date)
     return Response(json.dumps(data), mimetype='application/json')
+
+
+# Endpoint to retrieve log data in JSON format
+@app.route('/api/status', methods=['GET'])
+def fetch_status():
+    return render_template('log_table.html', log=read_log_file())
 
 
 # If file is run directly - ie. python server.py - then start server
